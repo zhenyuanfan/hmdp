dec396a34eb05dec882e94685f3a145c1258cb34
jiaowolf <2249472040@qq.com>
2025-11-19 15:19:39 +0800

    第二十一节：分布式锁-Redis分布式锁误删问题

--- hm-dianping/src/main/java/com/hmdp/utils/IlockImpl.java
+++ hm-dianping/src/main/java/com/hmdp/utils/IlockImpl.java
@@ -1,5 +1,7 @@
 package com.hmdp.utils;
 
+
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
@@ -9,22 +11,31 @@ public class IlockImpl implements Ilock {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
     private final String name;
-    private final String KEY = "lock:";
 
-    public IlockImpl(StringRedisTemplate stringRedisTemplate,String name){
+    public IlockImpl(StringRedisTemplate stringRedisTemplate, String name) {
         this.stringRedisTemplate = stringRedisTemplate;
         this.name = name;
     }
-    
+
+    private final String KEY = "lock:";
+    private final String UUID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    String s1 = Thread.currentThread().getId() + UUID_PREFIX;
+
     @Override
     public boolean tryLock(long timeoutSec) {
-        String id = Thread.currentThread().getId() + "";
+        String id = s1;
         Boolean b = stringRedisTemplate.opsForValue().setIfAbsent(KEY + name, id, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(b);
     }
 
     @Override
     public void unLock() {
-        stringRedisTemplate.delete(KEY + name);
+
+        String id = s1;
+        String s = stringRedisTemplate.opsForValue().get(KEY + name);
+        if (id.equals(s)) {
+            stringRedisTemplate.delete(KEY + name);
+        }
+
     }
 }
